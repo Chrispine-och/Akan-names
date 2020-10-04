e71fdc54628b91797d88a5ca3a486ef6930683c4
chrispine-och <chrisochieng150@gmail.com>
2020-10-04 12:32:10 +0300

    added a variable and a function

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var akanMaleNames = [ "Kwasi","Kwadwo","Kwabena","Kwaku","Yaw" ,"Kofi" ,"Kwame"]
+var akanFemaleNames = ["Akosua" ,"Adwoa","Abenaa","Akua","Yaa","Afua","Ama" ]
+function searchnames() {
+    var year = parseInt(document.getElementById("year").value)
+    var month = parseInt(document.getElementById("month").value)
+    var date = parseInt(document.getElementById("date").value)
+    
+
+}
